e438fb6804fa487e1e56d03ae26a9da09db1fe0b
Samuel Ofori <oforisml@gmail.com>
2024-12-14 09:11:35 +0000

    feat: Added pattern, policy and subscription-filter to cloudwatch

--- src/aws/cloudwatch/index.ts
+++ src/aws/cloudwatch/index.ts
@@ -1,3 +1,6 @@
 export * from "./log-group";
 export * from "./log-stream";
 export * from "./data-protection-policy";
+export * from "./policy";
+export * from "./pattern";
+export * from "./subscription-filter";
